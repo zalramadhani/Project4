42b4231638b7eebc8035c34532b32f11970c937f
zaidalramadhani <zaidalramadhani@localhost>
2025-01-29 21:08:00 -0500

    Added 2 variables and NEW add() method

--- src/Demo1/DemoTest4.java
+++ src/Demo1/DemoTest4.java
@@ -2,6 +2,20 @@ package Demo1;
 
 public class DemoTest4 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("ZAID & SALLY LIVE IN TAMPA");
+		
+		int Sally = 31; 
+		int Zaid = 36; 
+		
+		int result = Zaid + Sally; 
+		
+		DemoTest4 b = new DemoTest4 (); 
+		b.add("Zaid", 36);
+		
+		
+	}
+	
+	public void add (String name, int a) { 
+		System.out.println(name + a);
 	}
 }
